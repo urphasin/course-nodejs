b5a2adadc04b67a0966ed6a5b2135cab024830e8
Otito Amuga <urphasin@gmail.com>
2026-02-21 18:42:53 -0700

    p

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import express from "express";
 
 const app = express();
+const port = 3000;
 
-app.
+app.get('/', (req, res) => {
+    res.send("Whats the word.")
+})
 
+app.listen(port, () => {
+    console.log(`http://127.0.0.1:${port}`);
+})
